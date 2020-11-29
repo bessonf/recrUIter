2654d1dab1825e7d5f0edae03564539ff72959b8
Frank Besson <32044707+bessonf@users.noreply.github.com>
2020-11-29 15:48:03 -0500

    fix path import and homepage jim photo

--- routes/questions.js
+++ routes/questions.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/1', (req, res) => {
 	res.sendFile(path.resolve('static/questions/1.html'))
